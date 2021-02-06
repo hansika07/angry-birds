2ec6974614d17fcc374c72c43084b148474e695c
hansika07 <77222579+hansika07@users.noreply.github.com>
2021-02-06 14:45:47 +0530

    added slingshot

--- sketch.js
+++ sketch.js
@@ -1,22 +1,25 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
-var bgImage;
-function preload(){
-    bgImage = loadImage("sprites/bg.png")
+var backgroundImg,platform;
+var bird, slingShot;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
 }
 
 function setup(){
-    
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
+
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -35,14 +38,14 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(230,180,80, PI/2);
+    slingShot = new SlingShot(bird.body,{x:200,y:100});
 }
 
 function draw(){
-    background(bgImage);
+    background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
@@ -59,4 +62,14 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
+    
+    slingShot.display();    
+}
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+
+}
+function mouseReleased(){
+slingShot.fly();
 }
